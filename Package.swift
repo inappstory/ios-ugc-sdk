9f4d65429dde9001245b9e1c83fd3663708a24ab
StPashik <stpashik@gmail.com>
2022-12-02 17:31:57 +0300

    RTL support; stability improvements;

--- Package.swift
+++ Package.swift
@@ -13,8 +13,8 @@ let package = Package(
     ],
     targets: [
         .binaryTarget(name: "InAppStoryUGC_SwiftUI",
-                      url: "https://cs.inappstory.com/iOS/InAppStoryUGC_SwiftUI-1.1.1.xcframework.zip",
-                      checksum: "3775ef281c21be845b0ac051a6d291d0456147118db1c974604fcebec57a705f"
+                      url: "https://cs.inappstory.com/iOS/InAppStoryUGC_SwiftUI-1.1.2.xcframework.zip",
+                      checksum: "832893c5c4fd0ece5b7534ccbd7c8e40ce3f1e2d1b16e0283933a4033276b3b4"
                 )
     ],
     swiftLanguageVersions: [.v5]
