77e87b22d0aaee77609402b3824afd973e959932
Pavel Eroshin <stpashik@gmail.com>
2023-08-11 18:08:06 +0300

    stability improvements

--- Package.swift
+++ Package.swift
@@ -13,10 +13,9 @@ let package = Package(
     ],
     targets: [
         .binaryTarget(name: "InAppStoryUGC_SwiftUI",
-                      url: "https://github.com/inappstory/ios-ugc-sdk/files/11957288/InAppStoryUGC_SwiftUI-1.2.2.xcframework.zip",
-                      checksum: "efd80521aafefac27cddf0404270ea448722552a39ef8339f228144fbd21987f"
+                      url: "https://github.com/inappstory/ios-ugc-sdk/files/12322801/InAppStoryUGC_SwiftUI-1.2.3.xcframework.zip",
+                      checksum: "2d389f6b1445a25a83418aeb19fb0f475a662520616cbd9e4a0bd7725509459d"
                 )
     ],
     swiftLanguageVersions: [.v5]
 )
-
